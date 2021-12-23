890622094a80e5a294cbdb4931aa3098d9f5cafd
Lucifer-morning-star-96 <MSnell3130@student.egcc.edu>
2021-12-23 18:23:15 +0530

    Track overlay

--- device.mk
+++ device.mk
@@ -16,7 +16,9 @@ TARGET_SCREEN_WIDTH := 1080
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pe
+    $(LOCAL_PATH)/overlay-corvus \
+    $(LOCAL_PATH)/overlay-lineage 
+    
 
 # Display Device Config
 PRODUCT_COPY_FILES += \
